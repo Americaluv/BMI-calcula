b6678314680d06a4a37455bf77d3853cd8dfe434
Americaluv <118681051+Americaluv@users.noreply.github.com>
2023-01-19 01:38:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
-
-function kakeru(f){
-    f.bmi.value=parseFloat(f.A.value)/parseFloat(f.B.value)**2}
+'use strict';
+let height;
+let weight;
+function kakeru(f)
+{f.sintyo2.value=parseFloat(f.height.value) * parseFloat(f.height.value)
+    f.bmi.value=parseFloat(f.weight.value) / parseFloat(f.sintyo2.value) 
+}
