87cedec1bcc0d1ed1aba7329a3ae4ee49cc7acab
Americaluv <118681051+Americaluv@users.noreply.github.com>
2023-01-18 21:27:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-document.getElementById("height");
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const taijyu2 = document.getElementById('taijyu2');
+taijyu2.value = Number(height.value) * Number(height.value);
+const answer = weight / taijyu2;
 
-function taijyu2() {
-    height * height;
-}
 
-function bmi() {
-    weight / taijyu2;
-}
